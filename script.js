2b0b3979d6a5c66f3cb08feb7b275c57e94e9a3b
iikim <iikim2511@gmail.com>
2020-12-05 11:13:03 -0500

    zzzzzz

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // ●
 
-// ● C 02 자바스크립트의 목적과 셀렉터
+//🦄c2 자바스크립트의 목적과 셀렉터
 console.log("C 02 자바스크립트의 목적과 셀렉터");
 
 //  = 오른쪽을 왼쪽에 넣어주세요 라는 뜻 . 같다는 뜻이 아님
@@ -9,14 +9,14 @@ console.log("C 02 자바스크립트의 목적과 셀렉터");
 document.getElementById("test").innerHTML = "바보😛";
 document.getElementById("test").style.color = "red";
 
-// ● C 03 onclick 속성으로 버튼 기능개발 : Alert 박스 만들기
+// 🦄c3 onclick 속성으로 버튼 기능개발 : Alert 박스 만들기
 console.log("● C 03 onclick 속성으로 버튼 기능개발 : Alert 박스 만들기");
 
 // document.getElementById('alert').style.display = 'block'
 
 
 
-// 🦄04 function 문법으로 코드 줄이기/ 에러 체크하기
+// 🦄c4 function 문법으로 코드 줄이기/ 에러 체크하기
 console.log("●C 04 ");
 
 // function: 긴 js문장을 한단어로 축약,여러번 재사용 할때
