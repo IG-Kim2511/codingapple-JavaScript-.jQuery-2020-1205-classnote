0ed499a50c10bda1d309cc680c9c3ed66a52dc59
iikim <iikim2511@gmail.com>
2020-12-06 11:04:14 -0500

    c5  wip

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 //🦄c2 자바스크립트의 목적과 셀렉터
 
 //  "=" 뜻 : 오른쪽을 왼쪽에 넣어주세요 라는 뜻 . 같다는 뜻이 아님
-// selector, method
+// selector, method 👉 구글링해서 필요한것 찾기
 
 document.getElementById("test").innerHTML = "디즈니 innerHTML✋";
 
@@ -22,11 +22,50 @@ document.getElementById("test").style.color = "red";
 
 // 팁 : 콘솔창의 error메시지 : 그대로 복사해서 구글링하면 해결 가능
 
-function 안내창닫기(){
+function alert닫기(){
     document.getElementById('alert').style.display = 'none';
   }
-
-  안내창닫기();
+alert닫기();
 
   
-// 🦄5 parameter : function을 쓰는 이유 (function의 두번째 기능)
+// 🦄c5 parameter : function을 쓰는 이유 (function의 두번째 기능)
+
+
+// js 5-1) function 2개 만듬
+function alert열기5(){
+  document.getElementById('alert5').style.display = 'block';
+}
+
+function alert닫기5(){
+  document.getElementById('alert5').style.display = 'none';
+}
+
+// js 5-2) parameter 쓰기 
+/*
+함수실행(a)👉function(a)👉 a를 function안에서 자유롭게 사용함
+
+parameter 장점 : 함수1개로 parameter만 바꾸면서 여러기능 만듬*/  
+
+function ig5(파라미터){
+  document.getElementById('alert5').style.display=파라미터;
+}
+
+// ig5('파라미터');
+// ig5(123);
+
+// ig5('block');
+// ig5('none');
+
+
+// js 5-3) 
+/* 
+parameter 여러개 사용가능
+사용할 곳 지정해서 집어넣으면 끝 */
+
+function ig5-2(파라미터1,파라미터2){
+  document.getElementById(파라미터2).style.display=파라미터1;
+}
+
+ig5-2('파라미터1','파라미터2');
+
+
